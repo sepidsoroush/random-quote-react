26b0ad3a6a0db885cdaf46a5c41bd2ab6632fefe
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-20 21:10:21 +0200

    use useState to replace the elements and styles

--- src/App.js
+++ src/App.js
@@ -7,43 +7,47 @@ import { faTwitter , faTumblr} from '@fortawesome/free-brands-svg-icons'
 import './App.css';
 const data = require("./data.json");
 function App() {
-  let quote = "";
-  let author = '';
+  let text = "";
+  let writer = '';
   let randomIndex;
   let randomColor;
   let randomQuote;
   let randomAuthor;
-  quote = data.quotes[0].quote;
-  author = data.quotes[0].author;
-
+  text = data.quotes[0].quote;
+  writer = data.quotes[0].author;
+  const [quote , setQuote] = useState(text);
+  const [author , setAuthor] = useState(writer);
+  const [ color , setColor] = useState("black");
     function GetQuote() {
       //chnage quote
       randomIndex = Math.floor(Math.random()*(data.quotes.length));
       randomQuote = data.quotes[randomIndex].quote;
       randomAuthor = data.quotes[randomIndex].author;
-      const [quote , setQuote] = useState(quote);
-      const [author , setAuthor] = useState(author);
-      
+      setQuote(randomQuote);
+      setAuthor(randomAuthor);
+
       // change color
       randomColor = "#"+(Math.floor(Math.random()*16777215).toString(16));
+      setColor(randomColor);
+
     }
 
   return (
-    <div className="App">
-      <div id="wrapper">
+    <div className="App" style={{backgroundColor : `${color}`}}>
+      <div id="wrapper" >
         <div id="quote-box">
-          <div className="text-container" style={{color : `${randomColor}`}}>
+          <div className="text-container" style={{color : `${color}`}}>
             <span><FontAwesomeIcon icon={faQuoteLeft} className="quote-left" /></span><span id="text" >{quote}</span>
           </div>          
-          <div className="author-container" style={{color : `${randomColor}`}}>
+          <div className="author-container" style={{color : `${color}`}}>
             <span>- </span><span id="author">{author}</span>
           </div>
           <div className="button-container">
             <div className='media'>
-              <a href="https://twitter.com/intent/tweet" target="_blank" className='button' id="tweet-quote" style={{backgroundColor : `${randomColor}`}}><FontAwesomeIcon icon={faTwitter} /></a>
-              <a href="" target="_blank" id="tmblr-quote" className='button' style={{backgroundColor : `${randomColor}`}}><FontAwesomeIcon icon={faTumblr} /></a>
+              <a href="https://twitter.com/intent/tweet" target="_blank" className='button' id="tweet-quote" style={{backgroundColor : `${color}`}}><FontAwesomeIcon icon={faTwitter} /></a>
+              <a href="https://www.tumblr.com/" target="_blank" id="tmblr-quote" className='button' style={{backgroundColor : `${color}`}}><FontAwesomeIcon icon={faTumblr} /></a>
             </div>
-            <button id="new-quote" className='button' style={{backgroundColor : `${randomColor}`}} onClick={()=> setQuote(randomQuote)} >New quote</button>
+            <button id="new-quote" className='button' style={{backgroundColor : `${color}`}} onClick={()=> GetQuote()} >New quote</button>
           </div>
         </div>
         <div className="footer">
