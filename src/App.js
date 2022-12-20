dc86fb8c19cb20873da86d7daa2e52fb34da9aa5
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-20 10:01:58 +0200

    create DOM and instal fas package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrapper">
+        <div id="quote-box">
+          <div id="text">
+            <p></p>
+          </div>          
+          <div id="author"></div>
+          <div className="button">
+            <div className="media">
+            <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote"></a>
+            <a href="" id="tmblr-quote"></a>
+            </div>
+            <button id="new-quote">New quote</button>
+          </div>
+        </div>
+        <div className="footer">
+          <p>by <a href="https://github.com/sepidsoroush">SepidSoroush</a></p>
+        </div>
+      </div>
     </div>
   );
 }
