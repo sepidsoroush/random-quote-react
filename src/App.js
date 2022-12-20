f53c45c34b43b2e557cea55a4a7d918ed084108a
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-20 10:26:45 +0200

    instal fontawesome package and put icons in DOM

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter , faTumblr} from '@fortawesome/free-brands-svg-icons'
+
+
 import './App.css';
 
 function App() {
@@ -8,13 +13,14 @@ function App() {
       <div id="wrapper">
         <div id="quote-box">
           <div id="text">
+            <FontAwesomeIcon icon={faQuoteLeft} />
             <p></p>
           </div>          
           <div id="author"></div>
           <div className="button">
             <div className="media">
-            <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote"></a>
-            <a href="" id="tmblr-quote"></a>
+            <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="" id="tmblr-quote"><FontAwesomeIcon icon={faTumblr} /></a>
             </div>
             <button id="new-quote">New quote</button>
           </div>
